aed310d8ca3c1a7a448ef35df18e169bc24f2b67
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-08-27 15:13:36 -0500

    Surround in `mod` keyword

--- src/best-fit.rs
+++ src/best-fit.rs
@@ -1,19 +1,21 @@
 use std::cmp::PartialOrd;
 
-pub fn mean<T>(list: &[T]) -> T where T: PartialOrd {
-    list.iter().sum() / list.len()
-}
+mod bf {
+    pub fn mean<T>(list: &[T]) -> T where T: PartialOrd {
+        list.iter().sum() / list.len()
+    }
 
-pub fn bf_slope<T>(x: &[T], y: &[T], mean_x: T, mean_y: T) -> T where T: PartialOrd {
-    let nums: &[(T, T)] = x.iter().zip(y.iter());
-    nums.iter().fold(0, |acc: T, c: (T, T)| -> T {
-        acc + (c.1 - mean_x) * (c.2 - mean_y)
-    }) / nums.iter().fold(0, |acc: T, c: (T, T)| -> T {
-        let cc: T = (c.1 - mean_x);
-        acc + (cc * cc)
-    })
-}
+    pub fn bf_slope<T>(x: &[T], y: &[T], mean_x: T, mean_y: T) -> T where T: PartialOrd {
+        let nums: &[(T, T)] = x.iter().zip(y.iter());
+        nums.iter().fold(0, |acc: T, c: (T, T)| -> T {
+            acc + (c.1 - mean_x) * (c.2 - mean_y)
+        }) / nums.iter().fold(0, |acc: T, c: (T, T)| -> T {
+            let cc: T = (c.1 - mean_x);
+            acc + (cc * cc)
+        })
+    }
 
-pub fn y_intercept<T>(mean_x: T, mean_y: T, slope: T) -> T where T: PartialOrd {
-    mean_y - slope * mean_x
+    pub fn y_intercept<T>(mean_x: T, mean_y: T, slope: T) -> T where T: PartialOrd {
+        mean_y - slope * mean_x
+    }
 }
