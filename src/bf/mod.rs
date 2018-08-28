a6302a79877a5e76bb3e58b28eef7512c327ae41
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-08-28 15:34:36 -0500

    Optimize `bf_slope` of `bf` module

--- src/bf/mod.rs
+++ src/bf/mod.rs
@@ -4,8 +4,10 @@ pub fn mean<T>(list: &[T]) -> T where T: PartialOrd {
     list.iter().sum() / list.len()
 }
 
-pub fn bf_slope<T>(x: &[T], y: &[T], mean_x: T, mean_y: T) -> T where T: PartialOrd {
+pub fn bf_slope<T>(x: &[T], y: &[T]) -> T where T: PartialOrd {
     let nums: &[(T, T)] = x.iter().zip(y.iter());
+    let mean_x: T = mean(x);
+    let mean_y: T = mean(y);
     nums.iter().fold(0, |acc: T, c: (T, T)| -> T {
         acc + (c.1 - mean_x) * (c.2 - mean_y)
     }) / nums.iter().fold(0, |acc: T, c: (T, T)| -> T {
