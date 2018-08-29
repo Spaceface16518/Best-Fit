17595fec0350746f5b13b4df73f0db95514f2542
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-08-29 09:52:40 -0500

    Make preliminary main method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod bf;
+
 fn main() {
-    println!("Hello, world!");
+    let x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let y = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+    let slope = bf::bf_slope(&x, &y);
+    let y_intercept = bf::y_intercept(bf::util::mean(&x), bf::util::mean(&y), slope);
+    println!("x: {:?} y: {:?} slope: {} y-intercept: {}", &x, &y, slope, y_intercept);
 }
