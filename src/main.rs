17d35e802b4e1ab0ff04d7da2a9b864a4a3da991
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-08-30 13:58:03 -0500

    Add explicit types to `main`

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod bf;
 
 fn main() {
-    let x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let y = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
-    let slope = bf::bf_slope(&x, &y);
-    let y_intercept = bf::y_intercept(bf::mean::mean(&x), bf::mean::mean(&y), slope);
+    let x: [i32; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let y: [i32; 10] = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+    let slope: i32 = bf::bf_slope(&x, &y);
+    let y_intercept: i32 = bf::y_intercept(bf::mean::mean(&x), bf::mean::mean(&y), slope);
     println!("x: {:?} y: {:?} slope: {} y-intercept: {}", &x, &y, slope, y_intercept);
 }
