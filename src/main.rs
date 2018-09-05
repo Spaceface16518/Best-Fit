ba5c1d3141b6f4121a74c1da23c19efbc6587fdd
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-09-05 17:48:26 -0500

    Use generic method for mean

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ fn main() {
     let weight = [9.8, 7.35, 4.9, 2.45, 19.6, 17.15, 14.7, 12.25];
     let avg_k = [2.5, 2.3, 1.7, 1.2, 4.8, 4.4, 3.9, 2.7];
     let slope = bf::bf_slope(&weight, &avg_k);
-    let y_intercept = bf::y_intercept(bf::mean::f_mean(&weight[..]), bf::mean::f_mean(&avg_k), slope);
+    let y_intercept = bf::y_intercept(bf::mean::mean(&weight[..]), bf::mean::mean(&avg_k), slope);
     println!("weight: {:?} avg_k: {:?}\nslope: {} y-intercept: {}", &weight, &avg_k, slope, y_intercept);
 }
