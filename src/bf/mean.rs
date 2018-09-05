20f9e2a0e1ec0e7d43377630121eed4874871b6b
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-09-05 11:28:49 -0500

    Add `Copy` trait to `mean`

--- src/bf/mean.rs
+++ src/bf/mean.rs
@@ -2,7 +2,7 @@ use std::ops;
 use super::num;
 
 pub fn mean<T>(list: &[T]) -> T
-    where T: num::Num + ops::Add<T, Output=T> {
+    where T: num::Num + ops::Add<T, Output=T> + From<usize> + Clone {
     let sum: T = {
         let mut sum: T = T::zero();
         for &i in list.iter() {
