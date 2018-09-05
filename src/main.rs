10177b9cd792f041525ea29bb070cf1d023198bc
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-09-05 17:40:11 -0500

    Use generic method for slope

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod bf;
 fn main() {
     let weight = [9.8, 7.35, 4.9, 2.45, 19.6, 17.15, 14.7, 12.25];
     let avg_k = [2.5, 2.3, 1.7, 1.2, 4.8, 4.4, 3.9, 2.7];
-    let slope = bf::f_bf_slope(&weight, &avg_k);
+    let slope = bf::bf_slope(&weight, &avg_k);
     let y_intercept = bf::y_intercept(bf::mean::f_mean(&weight[..]), bf::mean::f_mean(&avg_k), slope);
     println!("weight: {:?} avg_k: {:?} slope: {} y-intercept: {}", &weight, &avg_k, slope, y_intercept);
 }
