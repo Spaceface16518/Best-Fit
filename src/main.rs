f4a01b9feec344b517e422d34ec84be00e297096
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-09-05 19:38:29 -0500

    Add static friction

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,15 @@ mod bf;
 fn main() {
     let weight = [9.8, 7.35, 4.9, 2.45, 19.6, 17.15, 14.7, 12.25];
     let avg_k = [2.5, 2.3, 1.7, 1.2, 4.8, 4.4, 3.9, 2.7];
-    let slope = bf::bf_slope(&weight, &avg_k);
-    let y_intercept = bf::y_intercept(bf::mean::mean(&weight[..]), bf::mean::mean(&avg_k), slope);
-    println!("weight: {:?} avg_k: {:?}\nslope: {} y-intercept: {}", &weight, &avg_k, slope, y_intercept);
+    let avg_s = [3.9, 3.6, 2.6, 1.7, 6.7, 7.3, 5.3, 4.4];
+    {
+        let slope = bf::bf_slope(&weight, &avg_k);
+        let y_intercept = bf::y_intercept(bf::mean::mean(&weight[..]), bf::mean::mean(&avg_k), slope);
+        println!("Kinetic:\n weight: {:?} avg_k: {:?}\n slope: {} y-intercept: {}", &weight, &avg_k, slope, y_intercept);
+    }
+    {
+        let slope = bf::bf_slope(&weight, &avg_s);
+        let y_intercept = bf::y_intercept(bf::mean::mean(&weight[..]), bf::mean::mean(&avg_s), slope);
+        println!("Static:\n weight: {:?} avg_s: {:?}\n slope: {} y-intercept: {}", &weight, &avg_s, slope, y_intercept);
+    }
 }
