d0546bb31eace3527ef049e2df21102596997d3c
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-09-11 16:51:00 -0500

    Specify imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use bf::bf_slope;
+use bf::mean::mean;
+use bf::y_intercept;
+
 mod bf;
 
 fn main() {
@@ -5,13 +9,13 @@ fn main() {
     let avg_k = [2.5, 2.3, 1.7, 1.2, 4.8, 4.4, 3.9, 2.7];
     let avg_s = [3.9, 3.6, 2.6, 1.7, 6.7, 7.3, 5.3, 4.4];
     {
-        let slope = bf::bf_slope(&weight, &avg_k);
-        let y_intercept = bf::y_intercept(bf::mean::mean(&weight[..]), bf::mean::mean(&avg_k), slope);
+        let slope = bf_slope(&weight, &avg_k);
+        let y_intercept = y_intercept(mean(&weight[..]), mean(&avg_k), slope);
         println!("Kinetic:\n weight: {:?} avg_k: {:?}\n slope: {} y-intercept: {}", &weight, &avg_k, slope, y_intercept);
     }
     {
-        let slope = bf::bf_slope(&weight, &avg_s);
-        let y_intercept = bf::y_intercept(bf::mean::mean(&weight[..]), bf::mean::mean(&avg_s), slope);
+        let slope = bf_slope(&weight, &avg_s);
+        let y_intercept = y_intercept(mean(&weight[..]), mean(&avg_s), slope);
         println!("Static:\n weight: {:?} avg_s: {:?}\n slope: {} y-intercept: {}", &weight, &avg_s, slope, y_intercept);
     }
 }
